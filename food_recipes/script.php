40cc4e1987f354b16ba0cb70dc7917b0190025f4
justdeepaktiwari <deepurajpandit2@gmail.com>
2021-06-29 22:58:08 +0530

    Create script.php

---
+++ food_recipes/script.php
@@ -0,0 +1,405 @@
+<script>
+    if (window.innerWidth > 1807) {
+
+        const cat1 = document.querySelectorAll(".cat-1");
+        const btn1 = document.querySelector("#btn1");
+
+        for (let i = 6; i < cat1.length; i++) {
+            cat1[i].style.display = "none";
+        }
+
+        btn1.addEventListener('click', () => {
+
+            for (let i = 6; i < cat1.length; i++) {
+                cat1[i].style.display = "block";
+            }
+
+            btn1.classList.remove("d-block");
+            btn1.style.display = "none";
+        });
+
+        const cat2 = document.querySelectorAll(".cat-2");
+        const btn2 = document.querySelector("#btn2");
+
+        for (let i = 6; i < cat1.length; i++) {
+            cat2[i].style.display = "none";
+        }
+
+        btn2.addEventListener('click', () => {
+
+            for (let i = 6; i < cat1.length; i++) {
+                cat2[i].style.display = "block";
+            }
+
+            btn2.classList.remove("d-block");
+            btn2.style.display = "none";
+        });
+        const cat3 = document.querySelectorAll(".cat-3");
+        const btn3 = document.querySelector("#btn3");
+
+        for (let i = 6; i < cat1.length; i++) {
+            cat3[i].style.display = "none";
+        }
+
+        btn3.addEventListener('click', () => {
+
+            for (let i = 6; i < cat1.length; i++) {
+                cat3[i].style.display = "block";
+            }
+
+            btn3.classList.remove("d-block");
+            btn3.style.display = "none";
+        });
+        const cat4 = document.querySelectorAll(".cat-4");
+        const btn4 = document.querySelector("#btn4");
+
+        for (let i = 6; i < cat1.length; i++) {
+            cat4[i].style.display = "none";
+        }
+
+        btn4.addEventListener('click', () => {
+
+            for (let i = 6; i < cat1.length; i++) {
+                cat4[i].style.display = "block";
+            }
+
+            btn4.classList.remove("d-block");
+            btn4.style.display = "none";
+        });
+        
+    } else if (window.innerWidth < 1807 && window.innerWidth > 1512) {
+        const cat1 = document.querySelectorAll(".cat-1");
+        const btn1 = document.querySelector("#btn1");
+
+        for (let i = 5; i < cat1.length; i++) {
+            cat1[i].style.display = "none";
+        }
+
+        btn1.addEventListener('click', () => {
+
+            for (let i = 5; i < cat1.length; i++) {
+                cat1[i].style.display = "block";
+            }
+
+            btn1.classList.remove("d-block");
+            btn1.style.display = "none";
+        });
+
+        const cat2 = document.querySelectorAll(".cat-2");
+        const btn2 = document.querySelector("#btn2");
+
+        for (let i = 5; i < cat1.length; i++) {
+            cat2[i].style.display = "none";
+        }
+
+        btn2.addEventListener('click', () => {
+
+            for (let i = 5; i < cat1.length; i++) {
+                cat2[i].style.display = "block";
+            }
+
+            btn2.classList.remove("d-block");
+            btn2.style.display = "none";
+        });
+        const cat3 = document.querySelectorAll(".cat-3");
+        const btn3 = document.querySelector("#btn3");
+
+        for (let i = 5; i < cat1.length; i++) {
+            cat3[i].style.display = "none";
+        }
+
+        btn3.addEventListener('click', () => {
+
+            for (let i = 5; i < cat1.length; i++) {
+                cat3[i].style.display = "block";
+            }
+
+            btn3.classList.remove("d-block");
+            btn3.style.display = "none";
+        });
+        const cat4 = document.querySelectorAll(".cat-4");
+        const btn4 = document.querySelector("#btn4");
+
+        for (let i = 5; i < cat1.length; i++) {
+            cat4[i].style.display = "none";
+        }
+
+        btn4.addEventListener('click', () => {
+
+            for (let i = 5; i < cat1.length; i++) {
+                cat4[i].style.display = "block";
+            }
+
+            btn4.classList.remove("d-block");
+            btn4.style.display = "none";
+        });
+        
+    } else if (window.innerWidth < 1512 && window.innerWidth > 1213) {
+        const cat1 = document.querySelectorAll(".cat-1");
+        const btn1 = document.querySelector("#btn1");
+
+        for (let i = 4; i < cat1.length; i++) {
+            cat1[i].style.display = "none";
+        }
+
+        btn1.addEventListener('click', () => {
+
+            for (let i = 4; i < cat1.length; i++) {
+                cat1[i].style.display = "block";
+            }
+
+            btn1.classList.remove("d-block");
+            btn1.style.display = "none";
+        });
+
+        const cat2 = document.querySelectorAll(".cat-2");
+        const btn2 = document.querySelector("#btn2");
+
+        for (let i = 4; i < cat1.length; i++) {
+            cat2[i].style.display = "none";
+        }
+
+        btn2.addEventListener('click', () => {
+
+            for (let i = 4; i < cat1.length; i++) {
+                cat2[i].style.display = "block";
+            }
+
+            btn2.classList.remove("d-block");
+            btn2.style.display = "none";
+        });
+        const cat3 = document.querySelectorAll(".cat-3");
+        const btn3 = document.querySelector("#btn3");
+
+        for (let i = 4; i < cat1.length; i++) {
+            cat3[i].style.display = "none";
+        }
+
+        btn3.addEventListener('click', () => {
+
+            for (let i = 4; i < cat1.length; i++) {
+                cat3[i].style.display = "block";
+            }
+
+            btn3.classList.remove("d-block");
+            btn3.style.display = "none";
+        });
+        const cat4 = document.querySelectorAll(".cat-4");
+        const btn4 = document.querySelector("#btn4");
+
+        for (let i = 4; i < cat1.length; i++) {
+            cat4[i].style.display = "none";
+        }
+
+        btn4.addEventListener('click', () => {
+
+            for (let i = 4; i < cat1.length; i++) {
+                cat4[i].style.display = "block";
+            }
+
+            btn4.classList.remove("d-block");
+            btn4.style.display = "none";
+        });
+        
+    } else if (window.innerWidth < 1213 && window.innerWidth > 914) {
+        const cat1 = document.querySelectorAll(".cat-1");
+        const btn1 = document.querySelector("#btn1");
+
+        for (let i = 3; i < cat1.length; i++) {
+            cat1[i].style.display = "none";
+        }
+
+        btn1.addEventListener('click', () => {
+
+            for (let i = 3; i < cat1.length; i++) {
+                cat1[i].style.display = "block";
+            }
+
+            btn1.classList.remove("d-block");
+            btn1.style.display = "none";
+        });
+
+        const cat2 = document.querySelectorAll(".cat-2");
+        const btn2 = document.querySelector("#btn2");
+
+        for (let i = 3; i < cat1.length; i++) {
+            cat2[i].style.display = "none";
+        }
+
+        btn2.addEventListener('click', () => {
+
+            for (let i = 3; i < cat1.length; i++) {
+                cat2[i].style.display = "block";
+            }
+
+            btn2.classList.remove("d-block");
+            btn2.style.display = "none";
+        });
+        const cat3 = document.querySelectorAll(".cat-3");
+        const btn3 = document.querySelector("#btn3");
+
+        for (let i = 3; i < cat1.length; i++) {
+            cat3[i].style.display = "none";
+        }
+
+        btn3.addEventListener('click', () => {
+
+            for (let i = 3; i < cat1.length; i++) {
+                cat3[i].style.display = "block";
+            }
+
+            btn3.classList.remove("d-block");
+            btn3.style.display = "none";
+        });
+        const cat4 = document.querySelectorAll(".cat-4");
+        const btn4 = document.querySelector("#btn4");
+
+        for (let i = 3; i < cat1.length; i++) {
+            cat4[i].style.display = "none";
+        }
+
+        btn4.addEventListener('click', () => {
+
+            for (let i = 3; i < cat1.length; i++) {
+                cat4[i].style.display = "block";
+            }
+
+            btn4.classList.remove("d-block");
+            btn4.style.display = "none";
+        });
+        
+    } else if (window.innerWidth < 914 && window.innerWidth > 633) {
+        const cat1 = document.querySelectorAll(".cat-1");
+        const btn1 = document.querySelector("#btn1");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat1[i].style.display = "none";
+        }
+
+        btn1.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat1[i].style.display = "block";
+            }
+
+            btn1.classList.remove("d-block");
+            btn1.style.display = "none";
+        });
+
+        const cat2 = document.querySelectorAll(".cat-2");
+        const btn2 = document.querySelector("#btn2");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat2[i].style.display = "none";
+        }
+
+        btn2.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat2[i].style.display = "block";
+            }
+
+            btn2.classList.remove("d-block");
+            btn2.style.display = "none";
+        });
+        const cat3 = document.querySelectorAll(".cat-3");
+        const btn3 = document.querySelector("#btn3");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat3[i].style.display = "none";
+        }
+
+        btn3.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat3[i].style.display = "block";
+            }
+
+            btn3.classList.remove("d-block");
+            btn3.style.display = "none";
+        });
+        const cat4 = document.querySelectorAll(".cat-4");
+        const btn4 = document.querySelector("#btn4");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat4[i].style.display = "none";
+        }
+
+        btn4.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat4[i].style.display = "block";
+            }
+
+            btn4.classList.remove("d-block");
+            btn4.style.display = "none";
+        });
+    } else {
+        const cat1 = document.querySelectorAll(".cat-1");
+        const btn1 = document.querySelector("#btn1");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat1[i].style.display = "none";
+        }
+
+        btn1.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat1[i].style.display = "block";
+            }
+
+            btn1.classList.remove("d-block");
+            btn1.style.display = "none";
+        });
+
+        const cat2 = document.querySelectorAll(".cat-2");
+        const btn2 = document.querySelector("#btn2");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat2[i].style.display = "none";
+        }
+
+        btn2.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat2[i].style.display = "block";
+            }
+
+            btn2.classList.remove("d-block");
+            btn2.style.display = "none";
+        });
+        const cat3 = document.querySelectorAll(".cat-3");
+        const btn3 = document.querySelector("#btn3");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat3[i].style.display = "none";
+        }
+
+        btn3.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat3[i].style.display = "block";
+            }
+
+            btn3.classList.remove("d-block");
+            btn3.style.display = "none";
+        });
+        const cat4 = document.querySelectorAll(".cat-4");
+        const btn4 = document.querySelector("#btn4");
+
+        for (let i = 2; i < cat1.length; i++) {
+            cat4[i].style.display = "none";
+        }
+
+        btn4.addEventListener('click', () => {
+
+            for (let i = 2; i < cat1.length; i++) {
+                cat4[i].style.display = "block";
+            }
+
+            btn4.classList.remove("d-block");
+            btn4.style.display = "none";
+        });
+        
+    }
+    </script>
